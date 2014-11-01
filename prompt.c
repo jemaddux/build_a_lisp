ec277bdd72b14255bdc301771156ac8f582b2286
John Maddux <JEMaddux@gmail.com>
2014-11-01 12:18:56 -0400

    A better repl

--- prompt.c
+++ prompt.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Declare a static biffer for user input of maximum size 2048
-static char input[2048];
+#include <editline/readline.h>
+// #include <editline/history.h>
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
+
   // Print Version and Exit Information
   puts("Lispy Version 0.0.0.0.1");
   puts("Press Ctrl-c to Exit\n");
 
   // In a never ending loop
-  while(1){
-    // Output our prompt
-    fputs("lispy> ", stdout);
+  while (1) {
 
-    // Read a line of user input of maximum size 2048
-    fgets(input, 2048, stdin);
+    // Output our prompt
+    char* input = readline("lispy> ");
+
+    // Add input to history
+    add_history(input);
 
     // Echo input back to user
     printf("No you're a %s", input);
+
+    // Free retrived input
+    free(input);
   }
 
   return 0;
