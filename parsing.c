12473b41b2ecd3b1a7c87e42bb201deb48394dfe
John Maddux <JEMaddux@gmail.com>
2014-11-27 20:26:30 -0500

    Chapter 7

--- parsing.c
+++ parsing.c
@@ -20,15 +20,44 @@ void add_history(char* unused) {}
 // #include <editline/history.h>
 #endif
 
+// Use opeator string to see which operation to perform
+long eval_op(long x, char* op, long y){
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  // If tagged as number return it directly
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  // The operator is always second child
+  char* op = t->children[1]->contents;
+
+  // We store the third child in 'x'
+  long x = eval(t->children[2]);
+
+  // Iterate the remaining children and combining
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")){
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv){
 
-  // Create Some Parsers
   mpc_parser_t* Number   = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
   mpc_parser_t* Expr     = mpc_new("expr");
   mpc_parser_t* Lispy    = mpc_new("lispy");
 
-  // Define them with the following language
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
       number   : /-?[0-9]+/ ;                             \
@@ -38,23 +67,19 @@ int main(int argc, char** argv){
     ",
     Number, Operator, Expr, Lispy);
 
-  // Print Version and Exit information
   puts("Lispy Version 0.0.0.0.2");
   puts("Press Ctrl-c to Exit\n");
 
-  // Never ending loop
   while (1){
     char* input = readline("lispy> ");
     add_history(input);
 
-    // Attempt to parse the user input
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      // On success print and delete the AST
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
-      // Otherwise print and delete the Error
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
@@ -62,7 +87,6 @@ int main(int argc, char** argv){
     free(input);
   }
 
-  // Undefine adn delete out parsers
   mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
   return 0;
