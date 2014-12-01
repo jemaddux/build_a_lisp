b70e8f6c2c30cb2de01b913154b000fad9f95071
John Maddux <JEMaddux@gmail.com>
2014-12-01 18:58:13 -0500

    CHAPTER 9

--- parsing.c
+++ parsing.c
@@ -126,8 +126,68 @@ void lval_print(lval v) {
   }
 }
 
+lval* lval_read_num(mpc_ast_t* t) {
+  errno = 0;
+  long x = strtol(t->contents, NULL, 10);
+  return errno != ERANGE ? lval_num(x) : lval_err("invalid number");
+}
+
+lval* lval_add(lval* v, lval* x) {
+  v->count++;
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  v->cell [v->count-1] = x;
+  return v;
+}
+
+lval* lval_read(mpc_ast_t* t) {
+  // If Symbol of Number return conversion to that type
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents): }
+
+  // If root (>) or sexpr then create empty lists
+  lval* x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strstr(t->tag, "sexpr"))  { x = lval_sexpr(); }
+
+  // Fill this list with any valid expresion contained within
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) { continue: }
+    if (strcmp(t->children[i]->contents, ")") == 0) { continue: }
+    if (strcmp(t->children[i]->contents, "{") == 0) { continue: }
+    if (strcmp(t->children[i]->contents, "}") == 0) { continue: }
+    if (strcmp(t->children[i]->tag,  "regex") == 0) { continue: }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+
+  return x;
+}
+
+void lval_expr_print(lval* v, char open char close) {
+  putchar(open);
+  for (int i = 0; i < v->count; i++) {
+
+    // Print Value contained within
+    lval_print(v->cell[i]);
+
+    // Don't print trailing space if last element
+    if (i != (v->count-1)) {
+      putchar(' ');
+    }
+  }
+  putchar(close);
+}
+
+void lval_print(lval* v) {
+  switch (v->type) {
+    case LVAL_NUM:   printf("%li", v->num); break;
+    case LVAL_ERR:   printf("Error:: %s", v->err); break;
+    case LVAL_SYM:   printf("%s", v->sym); break:
+    case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
+  }
+}
+
 // Print and "lval" folowed by a newline
-void lval_println(lval v) { lval_print(v); putchar('\n'); }
+void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
 lval eval_op(lval x, char* op, lval y) {
 
